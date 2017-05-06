7f27a507e9a7581d350503691a61a64466b0d58b
Sergio Garcia <sergio@garciparedes.me>
2017-05-06 14:38:50 +0200

    Minor changes

--- src/adaline_k.m
+++ src/adaline_k.m
@@ -5,13 +5,7 @@
 # Author: García Prado, Sergio
 # Author: Fernández Angulo, Oscar
 
-function w = adaline_k(x, y, seasons=5000, alpha = 0.3)
-  # TODO
-  # TODO
-  # TODO
-  # TODO
-  # Change From 'Minimum Least Squares' Method to 'Delta Rule' Method
-  
+function w = adaline_k(x, y, seasons=5000, alpha = 0.1)
   w = zeros(size(x)(2), 1);
   for t = 1:seasons
     for i=1:size(x,1)
