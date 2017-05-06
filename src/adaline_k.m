4da721a6283d5c9464e96a492771af9914c59f61
Sergio Garcia <sergio@garciparedes.me>
2017-05-06 13:15:22 +0200

    Working on Adaline

--- src/adaline_k.m
+++ src/adaline_k.m
@@ -5,22 +5,20 @@
 # Author: García Prado, Sergio
 # Author: Fernández Angulo, Oscar
 
-function w = adaline_k(x, y)
+function w = adaline_k(x, y, seasons=5000, alpha = 0.3)
   # TODO
   # TODO
   # TODO
   # TODO
-# Change From 'Minimum Least Squares' Method to 'Delta Rule' Method
-
-
-  A = zeros(size(x)(2), size(x)(2));
-  B = zeros(size(x)(2), 1);
-  for i=1:size(x)(2)
-    for j=1:size(x)(2)
-      A(i,j)=sum(x(:,i).*x(:,j));
+  # Change From 'Minimum Least Squares' Method to 'Delta Rule' Method
+  
+  w = zeros(size(x)(2), 1);
+  for t = 1:seasons
+    for i=1:size(x,1)
+      p = (x(i,:) * w);
+      w = w + alpha * (y(i)-p) * x(i,:)';
     end
-    B(i,1)=sum(y.*x(:,i));
+    t
+    w
   end
-  w=inv(A)*B;
-
 end
